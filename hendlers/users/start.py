55bdeee757bb4b29656bbecef7dff7c948e9fb6d
Vadim <vadim.kozich@mail.ru>
2023-01-25 22:56:54 +0300

    start

--- hendlers/users/start.py
+++ hendlers/users/start.py
@@ -10,14 +10,18 @@ start_router = Router(name='start')
 
 @start_router.message(CommandStart())
 async def command_start(message: Message):
-    await message.delete(message)
-    user = User(pk=message.from_user.id)
-    try:
-        await user.save()
-    except IntegrityError:
-        text = f'Давно не виделись! {message.from_user.full_name}, что ты от меня хочешь?'
-    else:
-        text = f'{message.from_user.full_name}, приветствую тебя в клубе настольных и ролевых игр: \"Let\'s play\".' \
-               f'Я могу познакомить тебя с нашей коллекцией игр, зарегестрировать на ближайшую игру.' \
-               f'Помогу выбрать развлечение, для твоей вечерники или дня рождения'
+    await message.delete()
+    user = User(id=message.from_user.id)
+    # try:
+    #     await user.save()
+    # except IntegrityError:
+    #     text = f'Давно не виделись! {message.from_user.full_name}, что ты от меня хочешь?'
+    # else:
+    #     text = f'{message.from_user.full_name}, приветствую тебя в клубе настольных и ролевых игр: \"Let\'s play\".' \
+    #            f'Я могу познакомить тебя с нашей коллекцией игр, зарегестрировать на ближайшую игру.' \
+    #            f'Помогу выбрать развлечение, для твоей вечерники или дня рождения'
+    text = f'{message.from_user.full_name}, приветствую тебя в клубе настольных и ролевых игр: \"Let\'s play\".'\
+           f'Я могу познакомить тебя с нашей коллекцией игр, зарегестрировать на ближайшую игру.'\
+           f'Помогу выбрать развлечение, для твоей вечерники или дня рождения'
+
     await message.answer(text=text)
